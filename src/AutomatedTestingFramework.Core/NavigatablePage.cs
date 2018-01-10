e22a4d9b0c687c7f108feb82abb78b809a4461cb
xenobiasoft <shannon.stewart@outlook.com>
2018-01-10 12:57:34 -0700

    Updated post build events to package and deploy nuget packages to a local repo (#19)

--- src/AutomatedTestingFramework.Core/NavigatablePage.cs
+++ src/AutomatedTestingFramework.Core/NavigatablePage.cs
@@ -2,13 +2,13 @@ using AutomatedTestingFramework.Core.Driver;
 
 namespace AutomatedTestingFramework.Core
 {
-	public abstract class NavigatablePage : BasePage
+	public abstract class NavigatablePage<TPage> : BasePage where TPage : BasePage
 	{
 		protected NavigatablePage(IDriver driver)
 			: base(driver)
 		{}
 
-		public BasePage Go()
+		public NavigatablePage<TPage> Go()
 		{
 			Driver.Navigate(RelativeUrl);
 
